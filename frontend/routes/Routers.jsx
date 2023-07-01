84740bd260fcd02fe15ffdf875b040944542968f
Barnns <s.barnnita@gmail.com>
2023-07-01 21:32:34 +0545

    b

---
+++ frontend/routes/Routers.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Routers from "../routes/Routers";
+
+const Routers = () => {
+    return <>
+        <Header />
+            <main>
+                <Routers />
+            </main>
+        
+        <Footer />
+        </>
+   
+}
+
+export default Routers
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,7 +1,8 @@
 import "./app.css";
+import layout from "./layout/layout";
 
 function App() {
- 
+ return <Layout />
 }
 
 export default App
